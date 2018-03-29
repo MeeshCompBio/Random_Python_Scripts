c12cf798f495026fa38e2481482c81a8eebcb905
meeshcompbio <mich0391@umn.edu>
2018-03-29 14:11:29 -0500

    update comments

--- Cython/primes.pyx
+++ Cython/primes.pyx
@@ -1,23 +1,28 @@
 def primes(int nb_primes):
+    '''Returns the number of prime numbers you want'''
+    # declare vars
     cdef int n, i, len_p
     cdef int p[1000]
+
+    # limit the input number since this is just and example
     if nb_primes > 1000:
         nb_primes = 1000
 
-    len_p = 0  # The current number of elements in p.
+    # set var equal to zero for incremental loop
+    len_p = 0
     n = 2
     while len_p < nb_primes:
-        # Is n prime?
+        # check if it is a prime num
         for i in p[:len_p]:
             if n % i == 0:
                 break
 
-        # If no break occurred in the loop, we have a prime.
+        # If it can't divide then it is a prime num
         else:
             p[len_p] = n
             len_p += 1
         n += 1
 
-    # Let's return the result in a python list:
+    # Return python list
     result_as_list  = [prime for prime in p[:len_p]]
     return result_as_list
